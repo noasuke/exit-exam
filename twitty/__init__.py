027e027d887b84bb2a4ea42d408983e33fb63203
noasuke <noasuke@hotmail.com>
2024-09-16 12:55:09 +0700

    add package flask migrate

--- twitty/__init__.py
+++ twitty/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -7,6 +8,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///twittydb.sqlite'
 app.config['SECRET_KEY'] = b'diekdieikdiei90049olfk'
 
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 
 from twitty import routes, models
